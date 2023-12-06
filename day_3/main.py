fe91298aab5bbd69288e6c091a8d3d7789decbdd
alabeli <uraval@wisc.edu>
2023-12-06 16:43:57 -0600

    day 3 part 1 , initial

--- day_3/main.py
+++ day_3/main.py
@@ -4,11 +4,23 @@ def part_one(filename: str) -> int:
     with open(filename, encoding="utf8") as f:
         lines = [line.strip() for line in f.readlines()]
 
+    MaxX = len(lines[0])
+    MaxY = len(lines)
     sum = 0
+    x=0
+    special_char =[]
     for line in lines:
+        y=0
+        for ch in line:
+            if ch in "123456789.":
+                pass
+            else:
+                special_char.append((x,y))    
+            y+=1
+        x+=1
         
         sum+= 0
-   
+    print(special_char)
     return sum
     
 def part_two(filename: str) -> int:
@@ -24,7 +36,7 @@ def part_two(filename: str) -> int:
     return sum
 
 if __name__ == "__main__":
-    input_path = "./AoC/day_3/example.txt"
+    input_path = "/Users/uraval/Documents/GitHub/AoC/day_3/example.txt"
     print("---Part One---")
     print(part_one(input_path))
     print("---Part Two---")
